a13757674caa7f322053d2293afaefa7893582bd
aydinkaya81 <aydinkaya81@gmail.com>
2026-05-19 23:36:34 +0300

    Update supabase-config.js

--- supabase-config.js
+++ supabase-config.js
@@ -1,4 +1,4 @@
 window.P2P_SUPABASE = {
   url: "https://gknjhcoddsbfubpxpmkj.supabase.co",
-  anonKey: "sb_publishable_pCJzOnm-a0LDaj2qe4_KBQ_89RVq8vg",
+  anonKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImdrbmpoY29kZHNiZnVicHhwbWtpIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzkxMjU1NjMsImV4cCI6MjA5NDcwMTU2M30.bTFEb2DO__1eQmXjh-cBPhEs_0Rsvt94xvMB4Oc5HEs",
 };
